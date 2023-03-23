6678632cc9fb45759251925d601565c20e72622b
ellen1586 <128570679+ellen1586@users.noreply.github.com>
2023-03-23 19:40:52 +0900

    Add files via upload

---
+++ week2/secproject.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+
+public class secproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("첫번쨰 숫자: ");
+		double x = sc.nextDouble();
+		
+		System.out.print("두번째 숫자: ");
+		double y = sc.nextDouble();
+		
+		System.out.print("세번째 숫자: ");
+		double z = sc.nextDouble();
+		
+		
+		System.out.println(x + y + z);
+		
+		
+		
+	}
+
+}
