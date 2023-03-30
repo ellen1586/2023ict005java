05d697bb6137d7a0a75a7be8e16bd20d4568a406
ellen1586 <128570679+ellen1586@users.noreply.github.com>
2023-03-30 14:36:17 +0900

    Add files via upload

---
+++ week2/1008.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; 
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		double a,b;
+		
+		a = sc.nextDouble();
+		b = sc.nextDouble();
+		
+		
+		System.out.println(a / b);
+	}	
+		
+}
